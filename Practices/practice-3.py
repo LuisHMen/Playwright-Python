6c467971908abea853ab9f4f87bba40cb17b5a23
LuisHMen <luis_hmen@outlook.com>
2025-03-26 00:27:21 -0600

    Carpetas renombradas.

--- Practices/practice-3.py
+++
@@ -1,9 +0,0 @@
-# Calcular el pago de horas trabajadas.
-
-Nombre = input("Nombre: ")
-Horas = float(input("¿Cuántas horas trabajaste?: "))
-PagoxHora = float(input("Monto que cobras por hora: "))
-
-PagoTotal = Horas * PagoxHora
-
-print(Nombre + " el monto a pagar es: " + str(PagoTotal))
