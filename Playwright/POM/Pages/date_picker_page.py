4acc1baff0c9a535a6d541929f28f34c81df3cd6
LuisHMen <luis_hmen@outlook.com>
2025-07-10 11:06:34 -0600

    New page with two calendar types to automate.

--- Playwright/POM/Pages/date_picker_page.py
+++ Playwright/POM/Pages/date_picker_page.py
@@ -1,37 +1,28 @@
 from playwright.sync_api import expect
 
-class date_picker:
+class date_picker():
     def __init__(self, page):
         self.page = page
         # Elements
-        self.letters = page.locator('#onlyLetters')
-        self.alphanumeric = page.locator('#alphanumeric')
-        self.email = page.locator('#emailFormat')
-        self.url = page.locator('#urlFormat')
-        self.date = page.get_by_placeholder("Fecha")
-        self.submit_btn = page.get_by_role("button", name="Enviar")
-        self.confirmation_message = page.locator('#flashMessage')
+        self.range_field = page.locator('#range-date-calendar')
+        self.apply_btn = page.get_by_role('button', name="Apply")
+        self.date_field = page.locator('input#calendar')
     
-    def navigate(self):
-        self.page.goto('https://validaciones.rodrigovillanueva.com.mx/Campos_Dos_OK.html')
+    def navigate(self, url):
+        self.page.goto(url)
     
-    def enter_letters(self, text):
-        self.letters.fill(text)
-    
-    def enter_alphabetic_text(self, text):
-        self.alphanumeric.fill(text)
-    
-    def enter_email(self, email):
-        self.email.fill(email)
+    def select_date_range(self, start_date, end_date):
+        self.range_field.click()
+        self.page.get_by_role("cell", name=start_date, exact=True).first.click()
+        self.page.get_by_role("cell", name=end_date).first.click()
 
-    def enter_url(self, url):
-        self.url.fill(url)
+    def click_apply_btn(self):
+        self.apply_btn.click()
+
+    def confirm_date_range(self, date_range):
+        expect(self.range_field).to_have_value(date_range)
     
-    def enter_date(self, date):
-        self.date.fill(date)
-    
-    def submit_form(self):
-        self.submit_btn.click()
-     
-    def read_confirmation_message(self, message):
-        expect(self.confirmation_message).to_have_text(message)
+    def select_date(self, day):
+        self.date_field.click()
+        self.page.get_by_role("cell", name=day).click()
+        self.page.get_by_role("cell", name=day).press("Escape")
--- Playwright/POM/date_picker.py
+++ Playwright/POM/date_picker.py
@@ -1,22 +1,18 @@
-from playwright.sync_api import sync_playwright, Playwright
-from Playwright.POM.Pages.date_picker_page import date_picker
+from playwright.sync_api import Playwright
+from Pages.date_picker_page import date_picker
 
 def test_enter_values(playwright: Playwright):
 #with sync_playwright() as play:
-    browser = playwright.chromium.launch(headless=False, slow_mo=1000, args=["--start-maximized"])
+    browser = playwright.webkit.launch(headless=False, slow_mo=1000, args=["--start-maximized"])
     context = browser.new_context()
     page = context.new_page()
-    page.set_default_timeout(5000)
 
-    dt = date_picker(page)
-    dt.navigate()
-    dt.enter_letters("LuisHMen")
-    dt.enter_alphabetic_text("John117")
-    dt.enter_email("test.pruebas@pruebas.com")
-    dt.enter_url("https://example.com")
-    dt.enter_date("2025-06-29")
-    dt.submit_form()
-    dt.read_confirmation_message("Formulario enviado exitosamente")
+    dp = date_picker(page)
+    dp.navigate("https://qa-practice.netlify.app/calendar")
+    dp.select_date_range("7", "21")
+    dp.click_apply_btn()
+    dp.confirm_date_range("01/07/2018 - 01/21/2018")
+    dp.select_date("17")
 
     # ---------------------
     context.close()
