ca64bb814ee800f405a8a8f969c22fd90fcd07cf
LuisHMen <luis_hmen@outlook.com>
2025-07-20 01:56:51 -0600

    Relevant comments are added to understand better the Fixtures

--- Playwright/conftest.py
+++ Playwright/conftest.py
@@ -2,6 +2,21 @@ from playwright.sync_api import Playwright
 from POM.Pages.login_fixture_page import Login
 import pytest
 
+# A Python function becomes a pytest fixture when decorated with @pytest.fixture. 
+# This decorator signals to pytest that the function should be treated as a fixture.
+
+# Setup and Teardown:
+# Fixtures can manage both setup and teardown.
+# Code before a yield statement in a fixture function constitutes the setup phase.
+# Code after a yield statement constitutes the teardown phase, which executes after the test(s) that used the fixture have completed.
+
+# Scope:
+# Fixtures can have different scopes (e.g., function, class, module, session), 
+# determining how frequently they are executed and their lifetime.
+
+# Fixture function.
+# A function-scoped fixture runs before each test function that requests it.
+
 @pytest.fixture(scope="function")
 def set_up(playwright: Playwright):
     browser = playwright.chromium.launch(headless = False, slow_mo = 1000, args=["--start-maximized"])
@@ -18,6 +33,9 @@ def set_up(playwright: Playwright):
     yield page
     context.close()
     browser.close()
+
+# A session-scoped fixture runs once per pytest session.
+# This means you can keep a session active while tests are running.
 
 @pytest.fixture(scope="session")
 def session(playwright: Playwright):
