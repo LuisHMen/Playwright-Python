87659e62af3ebd292955165c099d24c0779341c4
LuisHMen <luis_hmen@outlook.com>
2026-02-03 18:00:17 -0600

    Operadores de asignación.

---
+++ Python/Asignacion/assigment_op.py
@@ -0,0 +1,21 @@
+"""
+Operadores de asignación.
+# Assiment operator: =, +=, -=, *=, /=, %=
+a = a + 10 can be written as a += 10
+"""
+a = 70
+
+a += 30 # a = a + 70
+print("a += example: ", a)
+
+a -= 20 # a = a - 20
+print("a -= example: ", a)
+
+a *= 2 # a = a * 2
+print("a *= example: ", a)
+
+a /= 8 # a = a / 8
+print("a /= example: ", a)
+
+a %= 6 # a = a % 6
+print("a %= example: ", a)
